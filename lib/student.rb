10c44de95b8de268ee27952134666ede552911c2
Olena Ageyeva <eageeva@hotmail.com>
2017-12-03 18:28:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,15 @@ class Student
    student
  end
 
+ def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    student=self.new
+    student.id=row[0]
+    student.name=row[1]
+    student.grade=row[2]
+    student
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

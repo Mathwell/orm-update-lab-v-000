b149b82bce808e5ce9842811d95784607fc2ab4e
Olena Ageyeva <eageeva@hotmail.com>
2017-12-03 18:33:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
  def new_from_db(row)
     # create a new Student object given a row from the database
     self.create(row[1],row[2])
-    self.id=row[0]
+    #self.id=row[0]
     self
   end
 
